6a8b2c76b3d7fd8765537057b3c1020c4f43c8f7
Vrezh Fedora <vrezhraiden@gmail.com>
2023-11-24 10:46:55 +0100

    style(post): updated styles for post editor

--- src/components/ArticleAdd/Article.tsx
+++ src/components/ArticleAdd/Article.tsx
@@ -8,7 +8,7 @@ import { ArticleDescription } from '@/components/ArticleAdd/ArticleDescription'
 export const Article: FC = () => {
   return (
     <div className='flex w-full flex-col items-center gap-12 mb-10'>
-      <h1 className='text-xl w-full max-w-screen-lg mx-auto text-white font-bold'>
+      <h1 className='text-xl w-full max-w-screen-lg mx-auto text-ctp-text font-bold'>
         ADD NEW ARTICLE
       </h1>
       <ArticleHeader />
--- src/components/ArticleAdd/UploadBlock.tsx
+++ src/components/ArticleAdd/UploadBlock.tsx
@@ -17,37 +17,37 @@ export const UploadBlock: React.FC<UploadBlockProps> = ({
   }
 
   return (
-    <div className='p-4 shadow rounded flex justify-between w-full gap-6'>
+    <div className='p-4 flex justify-between w-full gap-6'>
       <div className='flex flex-col gap-5'>
-        <div className='flex flex-col items-center justify-center w-[301px] h-[145px] p-4 border-2 border-dashed border-ctp-pink rounded text-slate-500'>
+        <div className='flex flex-col items-center justify-center w-[301px] h-[145px] p-4 border-2 border-dashed border-ctp-pink rounded text-ctp-subtext1'>
           <input
             type='file'
             onChange={handleImageChange}
             className='opacity-0 absolute cursor-pointer h-56 w-72 '
           />
-          <p className='font-bold text-xl'>Upload Image</p>
-          <p className='text-xs text-slate-500 italic'>
+          <p className='font-bold text-xl text-ctp-subtext1'>Upload Image</p>
+          <p className='text-xs text-ctp-subtext0 italic'>
             Max 1920x1080
           </p>
         </div>
 
         <div>
-          <p className='text-slate-500 pb-4'>Images Copyright</p>
+          <p className='text-ctp-subtext1 pb-4'>Images Copyright</p>
 
           <input
             type='text'
             placeholder="Image's Copyright"
-            className='rounded-full bg-white p-2 w-full placeholder:px-2'
+            className='rounded-full border focus:outline-none bg-ctp-crust text-ctp-text border-ctp-pink focus:border-ctp-lavender p-2 w-full placeholder:px-2'
           />
         </div>
       </div>
 
-      <div className=' h-full border-2 border-ctp-pink rounded' />
+      <div className=' h-full border border-ctp-pink rounded' />
 
       <div className='flex flex-col w-full gap-1.5'>
         <label
           htmlFor='title'
-          className='text-slate-500 font-bold px-2'
+          className='text-ctp-subtext1 font-bold px-2'
         >
           Title
         </label>
@@ -55,16 +55,16 @@ export const UploadBlock: React.FC<UploadBlockProps> = ({
           id='title'
           type='text'
           placeholder='Title'
-          className='mb-2 p-2 border rounded-full focus:outline-0 placeholder:px-4'
+          className='mb-2 p-2 rounded-full placeholder:px-4 bg-ctp-crust text-ctp-text focus:outline-none border-ctp-pink border focus:border-ctp-lavender'
         />
         <label
           htmlFor='author'
-          className='text-slate-500 font-bold px-2'
+          className='text-ctp-subtext1 font-bold px-2 '
         >
           Author
         </label>
         <select
-          className='mb-2 p-2 border rounded-full focus:outline-0 px-4'
+          className='mb-2 p-2 border rounded-full focus:outline-0 px-4 bg-ctp-crust text-ctp-text border-ctp-pink focus:border-ctp-lavender'
           id='author'
         >
           <option value='crossnetics'>Crossnetics</option>
@@ -72,12 +72,12 @@ export const UploadBlock: React.FC<UploadBlockProps> = ({
 
         <label
           htmlFor='tag'
-          className='text-slate-500 font-bold px-2'
+          className='text-ctp-subtext1 font-bold px-2'
         >
           Tag
         </label>
         <select
-          className='p-2 border rounded-full focus:outline-0 px-4'
+          className='p-2 border rounded-full focus:outline-0 px-4 bg-ctp-crust text-ctp-text border-ctp-pink focus:border-ctp-lavender'
           id='tag'
         >
           <option value='crossnetics'>Crossnetics</option>
