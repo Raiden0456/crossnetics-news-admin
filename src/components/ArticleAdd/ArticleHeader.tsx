39a02b9c1f66b2f53e2c918856e27fd02558c113
Vrezh Fedora <vrezhraiden@gmail.com>
2023-11-24 12:50:04 +0100

    style(post): style updates

--- src/components/ArticleAdd/ArticleHeader.tsx
+++ src/components/ArticleAdd/ArticleHeader.tsx
@@ -9,28 +9,28 @@ export const ArticleHeader: FC = () => {
   return (
     <div className='w-full max-w-screen-lg px-7 py-5 bg-ctp-mantle rounded-xl flex justify-between mx-auto'>
       <div className='flex justify-center items-center gap-3'>
-        <div className='text-ctp-text text-xs font-extrabold uppercase px-10 py-3 bg-ctp-surface1 rounded-xl'>
+        <div className='text-ctp-text text-xs font-extrabold uppercase px-10 py-3 bg-ctp-surface0 rounded-xl'>
           blog post
         </div>
 
-        <div className='text-ctp-text text-xs font-extrabold uppercase px-10 py-3 bg-ctp-surface1 rounded-xl'>
+        <div className='text-ctp-text text-xs font-extrabold uppercase px-10 py-3 bg-ctp-surface0 rounded-xl'>
           news entry
         </div>
       </div>
 
       <div className='flex justify-start items-center gap-5'>
         <div className='justify-start items-start flex'>
-          <div className='w-11 h-11 rounded-full border border-ctp-maroon flex justify-center items-center'>
+          <div className='cursor-pointer w-11 h-11 rounded-full transition-color duration-200 border border-ctp-maroon hover:bg-ctp-maroon flex justify-center items-center'>
             <Image src={basket} alt={'basket'} />
           </div>
         </div>
-        <div className='px-5 py-2.5 rounded-full border border-ctp-blue justify-center items-center flex'>
-          <div className="text-ctp-text text-xs font-extrabold font-['Montserrat'] uppercase">
+        <div className='cursor-pointer px-5 py-2.5 rounded-xl transition-all duration-200 border border-ctp-blue text-ctp-text hover:bg-ctp-blue hover:text-ctp-mantle hover:scale-105 justify-center items-center flex'>
+          <div className=" text-xs font-extrabold font-['Montserrat'] uppercase ">
             Preview
           </div>
         </div>
-        <div className='px-5 py-3 bg-ctp-blue rounded-xl justify-center items-center flex'>
-          <div className="text-ctp-mantle text-xs font-extrabold font-['Montserrat'] uppercase">
+        <div className='cursor-pointer px-5 py-3 transition-all duration-200 border border-ctp-blue text-ctp-text hover:bg-ctp-blue hover:text-ctp-mantle hover:scale-105 rounded-xl justify-center items-center flex'>
+          <div className=" text-xs font-extrabold font-['Montserrat'] uppercase">
             PUBLISH
           </div>
         </div>
--- src/components/ArticleAdd/UploadBlock.tsx
+++ src/components/ArticleAdd/UploadBlock.tsx
@@ -8,35 +8,51 @@ interface UploadBlockProps {
 export const UploadBlock: React.FC<UploadBlockProps> = ({
   onImageUpload,
 }) => {
+  const [imageUrl, setImageUrl] = React.useState<string | null>(null);
+
   const handleImageChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     if (e.target.files && e.target.files[0]) {
-      onImageUpload(e.target.files[0])
+      const file = e.target.files[0];
+      onImageUpload(file);
+      setImageUrl(URL.createObjectURL(file));
     }
-  }
+  };
 
   return (
     <div className='p-4 flex justify-between w-full gap-6'>
       <div className='flex flex-col gap-5'>
-        <div className='flex flex-col items-center justify-center w-[301px] h-[145px] p-4 border-2 border-dashed border-ctp-pink rounded text-ctp-subtext1'>
+      <div
+          className='flex flex-col items-center justify-center w-[301px] h-[145px] p-4 border-2 border-dashed border-ctp-pink rounded-2xl overflow-hidden text-ctp-subtext1'
+          style={{
+            backgroundImage: imageUrl ? `url(${imageUrl})` : 'none',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center'
+          }}
+        >
           <input
             type='file'
             onChange={handleImageChange}
-            className='opacity-0 absolute cursor-pointer h-56 w-72 '
+            className='opacity-0 absolute cursor-pointer h-56 w-72'
           />
-          <p className='font-bold text-xl text-ctp-subtext1'>Upload Image</p>
-          <p className='text-xs text-ctp-subtext0 italic'>
-            Max 1920x1080
-          </p>
+          {!imageUrl && (
+            <>
+              <p className='font-bold text-xl text-ctp-subtext1'>Upload Image</p>
+              <p className='text-xs text-ctp-subtext0 italic'>
+                Max 1920x1080
+              </p>
+            </>
+          )}
         </div>
 
         <div>
-          <p className='text-ctp-subtext1 pb-4'>Images Copyright</p>
+          <p className='text-ctp-subtext1 pb-4'>Copyright</p>
 
           <input
             type='text'
-            placeholder="Image's Copyright"
+            placeholder="Source"
             className='rounded-full border focus:outline-none bg-ctp-crust text-ctp-text border-ctp-pink focus:border-ctp-lavender p-2 w-full placeholder:px-2'
           />
         </div>
