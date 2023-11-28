60e1618b1d3a9108e90c4ce3a1a896270a73a42c
lincode <lincode@bk.ru>
2023-11-29 00:45:22 +0300

    style (articleBlock): fixed style in text-editor

--- src/components/ArticleAdd/ArticleBlock-2/ArticleBlock-2Description/ArticleDescriptionBlock_2.tsx
+++ src/components/ArticleAdd/ArticleBlock-2/ArticleBlock-2Description/ArticleDescriptionBlock_2.tsx
@@ -50,7 +50,7 @@ export const ArticleDescriptionBlock2 = () => {
           value={value}
           onChange={setValue}
           modules={moduleOptions}
-          className='h-fit mb-14 text-ctp-text placeholder:text-ctp-text'
+          className='h-fit mb-14 text-ctp-text'
           placeholder='Text Block'
         />
       </div>
